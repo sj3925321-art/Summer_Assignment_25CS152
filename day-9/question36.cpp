06fb3f8cbd8b0444fa5d9017ad977629da26e070
Satyam Kumar Jha <sj3925321@gmail.com>
2026-06-13 20:57:29 +0530

    Day-9 uploaded.

---
+++ day-9/question36.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n=5;
+    for(int i=0; i<=n; i++) {
+        for(int j=1; j<=n; j++) {
+        if(i==1 || i==n || j==1 || j==n) 
+            cout<<"*";
+        else
+        cout<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
