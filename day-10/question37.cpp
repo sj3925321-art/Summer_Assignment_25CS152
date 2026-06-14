5bb3fd3a9609e97f06bb20b5cd17bdaff6b5f00a
Satyam Kumar Jha <sj3925321@gmail.com>
2026-06-14 11:42:19 +0530

    Day-11 uploaded.

---
+++ day-10/question37.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n=5;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n-i; j++) {
+            cout<<" ";
+        }
+        for(int k=0; k<2*i+1; k++) {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
---
+++ day-10/question38.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n=5;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<i; j++) {
+            cout<<" ";
+        }
+        for(int k=1; k<2*(n-i); k++) {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
